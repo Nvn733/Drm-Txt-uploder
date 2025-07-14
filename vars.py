e23e7e1c61ff2c94bcaa8c38cedf587fc0383284
Nvn733 <shemsir785@gmail.com>
2025-07-14 21:39:58 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -3,17 +3,17 @@
 import os
 from os import environ
 
-API_ID = int(environ.get("API_ID", "xxxxxxxxx"))
-API_HASH = environ.get("API_HASH", "xxxxxxxxxxxxxxxxxxxxxxx")
-BOT_TOKEN = environ.get("BOT_TOKEN", "")
+API_ID = int(environ.get("API_ID", "29020892")
+API_HASH = environ.get("API_HASH", "15bfac43cfbeb3993c942ce2f11ec3f8")
+BOT_TOKEN = environ.get("BOT_TOKEN", "8096675089:AAFBP7q9ripjNz8oKIEk7szXDRKDX-dq8Ko")
 
-OWNER = int(environ.get("OWNER", "5680454765"))
+OWNER = int(environ.get("OWNER", "7068000043"))
 CREDIT = environ.get("CREDIT", "𝙎𝘼𝙄𝙉𝙄 𝘽𝙊𝙏𝙎")
 
-TOTAL_USER = os.environ.get('TOTAL_USERS', '5680454765').split(',')
+TOTAL_USER = os.environ.get('TOTAL_USERS', '7068000043').split(',')
 TOTAL_USERS = [int(user_id) for user_id in TOTAL_USER]
 
-AUTH_USER = os.environ.get('AUTH_USERS', '5680454765').split(',')
+AUTH_USER = os.environ.get('AUTH_USERS', '7068000043').split(',')
 AUTH_USERS = [int(user_id) for user_id in AUTH_USER]
 if int(OWNER) not in AUTH_USERS:
     AUTH_USERS.append(int(OWNER))
